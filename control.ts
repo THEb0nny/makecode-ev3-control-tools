63b8bbdc129db976b656d3bde0f81ebd1fd2d266
Dmitriy Antipov <aston_bonny@mail.ru>
2024-04-27 00:04:41 +0300

    Первоначальные файлы для проекта MakeCode

---
+++ control.ts
@@ -0,0 +1,19 @@
+namespace control {
+
+    /**
+     * A method to wait for the end of the cycle for a specified time.
+     * Метод для ожидания завершения цикла в течение заданного времени.
+     * @param startTime start time, eg: 0
+     * @param delay waiting time, eg: 10
+     */
+    //% blockId="PauseUntilTime"
+    //% block="starting from $startTime| wait for $delay|ms"
+    //% block.loc.ru="начиная от $startTime| ждать $delay|мс"
+    //% weight="6"
+    export function pauseUntilTime(startTime: number, ms: number) {
+        if (startTime == 0) startTime = control.millis();
+        const waitCompletionTime = startTime + ms;
+        while (control.millis() < waitCompletionTime) loops.pause(0.01);
+    }
+
+}
